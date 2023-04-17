04910c571d9626ba93afdc5cdc86f474b565e7ba
hyeri-woo <woohyeri0525@gmail.com>
2023-04-17 20:03:01 +0900

    0417 lecture

--- JavaScript/lecture-practice/date.js
+++ JavaScript/lecture-practice/date.js
@@ -65,13 +65,13 @@ today.toLocaleString('ja-JP'); // -> 2020/7/24 12:30:00
 
 
 const dayNames = [
-  '(일요일)',
-  '(월요일)',
-  '(화요일)',
-  '(수요일)',
-  '(목요일)',
-  '(금요일)',
-  '(토요일)'
+    '(일요일)',
+    '(월요일)',
+    '(화요일)',
+    '(수요일)',
+    '(목요일)',
+    '(금요일)',
+    '(토요일)'
 ];
 // getDay 메서드는 해당 요일(0 ~ 6)을 나타내는 정수를 반환한다.
 const day = dayNames[today.getDay()];
@@ -83,3 +83,20 @@ let hour = today.getHours();
 let minute = today.getMinutes();
 let second = today.getSeconds();
 const ampm = hour >= 12 ? 'PM' : 'AM';
+
+// UTC와 today의 지정 로캘(locale) KST와의 차이는 -9시간이다.
+// 쉬운 말로 협정 세계시(Universal Time Coordinated)와 내 컴퓨터 시간 차가 -9시간
+// https://ko.wikipedia.org/wiki/%ED%98%91%EC%A0%95_%EC%84%B8%EA%B3%84%EC%8B%9C
+today.getTimezoneOffset() / 60
+// UTC와 today의 지정 로캘(locale) KST와의 차이는 -9시간이다.
+// 쉬운 말로 협정 세계시(Universal Time Coordinated)와 내 컴퓨터 시간 차가 -9시간
+// https://ko.wikipedia.org/wiki/%ED%98%91%EC%A0%95_%EC%84%B8%EA%B3%84%EC%8B%9C
+// locale 을 활용하면 언어권에 맞게 입력과 출력을 수정하지 않고도 사용하는 언어권에 맞는 날짜를 처리할 수 있습니다.
+//UTC와 현재 로케일(호스트 시스템, today의 지정 로캘 KST(Korea Standard Time))의 차이는 -9시간이다.
+today.getTimezoneOffset() / 60
+
+// https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Date/toISOString
+// 국제표준시 기준 형식으로 표현
+today.toISOString();
+today.toISOString().slice(0, 10);
+today.toISOString().slice(0, 10).replace(/-/g, '')
