bc1f1df7bdb236d2b87f3bb58d84658f477ed676
hyeri-woo <woohyeri0525@gmail.com>
2023-05-02 19:55:25 +0900

    0502 lecture

---
+++ nodeJS/pet-sns/model/auth.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const authSchema = new Schema({
+    username: String, 
+    password: String,
+});
+
+module.exports = mongoose.model("auth", authSchema);
+
