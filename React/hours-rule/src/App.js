f6f76c11f1359f39037cc9def7d68c862ef54abb
hyeri-woo <woohyeri0525@gmail.com>
2023-05-08 22:35:44 +0900

    Add project

---
+++ React/hours-rule/src/App.js
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+import Landing from "./Landing.jsx"
+import './Landing.css';
+
+function App() {
+  return (
+    <Fragment>
+      <Landing/>
+    </Fragment>
+  );
+}
+export default App;
